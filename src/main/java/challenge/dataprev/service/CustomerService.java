77725f23642c65d7315bf4d6adbe70a0ea0ccedd
Marcos Amaral <marcos.professordelphi@gmail.com>
2020-10-17 17:48:55 -0300

    Crud Customer: creation Customer Repository

---
+++ src/main/java/challenge/dataprev/service/CustomerService.java
@@ -0,0 +1,28 @@
+package challenge.dataprev.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import challenge.dataprev.dto.CustomerRequestDto;
+import challenge.dataprev.entity.Customer;
+
+@Service
+public interface CustomerService {
+		
+	//all customer		
+	public List<Customer> getFindAll();
+	//customer
+	public Customer getCustomerById(Long id);   
+	//save customer
+	public  Customer save(CustomerRequestDto customerRequestDto);
+	//update customer
+	public void update(CustomerRequestDto CustomerRequestDto, Long id);
+	//delete customer
+	public void delete(Long id);   
+	
+	   		
+
+	}
+	
+
---
+++ src/main/java/challenge/dataprev/serviceimpl/CustomerServiceImpl.java
@@ -0,0 +1,41 @@
+package challenge.dataprev.serviceimpl;
+
+import java.util.List;
+
+import challenge.dataprev.dto.CustomerRequestDto;
+import challenge.dataprev.entity.Customer;
+import challenge.dataprev.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public List<Customer> getFindAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer getCustomerById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer save(CustomerRequestDto customerRequestDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(CustomerRequestDto CustomerRequestDto, Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
