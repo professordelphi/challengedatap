13fd3f6f4d9dbd754d9e129257dfc30f1e0acb71
Marcos Amaral <marcos.professordelphi@gmail.com>
2020-10-17 16:53:00 -0300

    Crud Customer: creation Customer Repository

--- src/main/java/challenge/dataprev/entity/Customer.java
+++ src/main/java/challenge/dataprev/entity/Customer.java
@@ -8,7 +8,12 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
-
+/*
+ * Marcos Amaral Dev Java
+ * 17-10-2020
+ * Challenge DataPrev
+ * Crud Customer 
+ * Class Customer*/
 @Data
 @Entity
 @Table(name = "CUSTOMER")
@@ -20,11 +25,10 @@ public class Customer {
 	@Column(name = "NAME", length = 50, nullable = false, unique = false)
 	private String name;
 
-	@Column(name = "CPF", length = 50, nullable = false, unique = false)
+	@Column(name = "CPF", length = 50, nullable = false, unique = true)
 	private String cpf;
 
 	@Column(name = "ADDRESS", length = 50, nullable = false, unique = false)
 	private String address;
-
 	
 }
