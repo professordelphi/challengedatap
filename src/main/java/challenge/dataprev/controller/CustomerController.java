ea0e322db9833dd0d1bbb5967536e32a5c3177c4
Marcos Amaral <marcos.professordelphi@gmail.com>
2020-10-17 22:32:36 -0300

    Crud Customer: adjust [other]

--- src/main/java/challenge/dataprev/controller/CustomerController.java
+++ src/main/java/challenge/dataprev/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package challenge.dataprev.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,9 +49,16 @@ public class CustomerController {
 	private  ResponseEntity<Customer> getCustomerById(@PathVariable("customerid") Long customerid)   
 	{  
 		
-		Customer customer = this.customerService.getCustomerById(customerid);
+		Optional<Customer> customer  = Optional.ofNullable(this.customerService.getCustomerById(customerid));
 		
-		return   ResponseEntity.ok(customer) ;  
+		if (!customer.isEmpty())
+		{
+			return   ResponseEntity.ok(customer.get()) ;
+			
+		}
+		
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		  
 	}  
 	
 	//It deletes a specified customer  
