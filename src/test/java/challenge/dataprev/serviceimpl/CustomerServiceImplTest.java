a6900d51ac0aa545f53dde1dba3c31b49fb9fa73
Marcos Amaral <marcos.professordelphi@gmail.com>
2020-10-26 17:10:23 -0300

    Crud Customer: refactor test 02

--- src/test/java/challenge/dataprev/serviceimpl/CustomerServiceImplTest.java
+++ src/test/java/challenge/dataprev/serviceimpl/CustomerServiceImplTest.java
@@ -32,13 +32,12 @@ public class CustomerServiceImplTest {
 	@Mock
 	private CustomerRepository customerRepository;
 
-	@Mock
-	File file;
+	
 
 	@Test(expected = Exception.class)
 	public void findAllTest() throws IOException {
 
-		//Popular dados
+		// Fill data
 
 		Customer custumer = new Customer();
 
@@ -47,28 +46,30 @@ public class CustomerServiceImplTest {
 		custumer.setName("Marcos");
 		custumer.setId(1l);
 		List<Customer> value = new ArrayList<Customer>();
-	//	value.add(custumer);
+
 
 		CustomerRequestDto customerRequestDto = new CustomerRequestDto();
-		
+
 		customerRequestDto.setName(custumer.getName());
 		customerRequestDto.setCpf(custumer.getCpf());
 		customerRequestDto.setAddress(custumer.getAddress());
 		customerRequestDto.setId(custumer.getId());
-		
+
 		this.customerServiceImpl.save(customerRequestDto);
-		
+
 		value = this.customerServiceImpl.getFindAll();
-		//exec
-		when(this.customerRepository.findAll()).thenReturn(value);
-       //verification
-		assertEquals("Marcos", value.get(0).getName());
 		
+		// exec
+		when(this.customerRepository.findAll()).thenReturn(value);
+		
+		// verification
+		assertEquals("Marcos", value.get(0).getName());
+
 	}
 
 	@Test(expected = Exception.class)
 	public void getCustomerById() {
-		// TODO Auto-generated method stub
+		// Fill data
 		Customer custumer = new Customer();
 
 		custumer.setAddress("Rua A");
@@ -76,18 +77,18 @@ public class CustomerServiceImplTest {
 		custumer.setName("Marcos");
 		custumer.setId(1l);
 
-		//exec
+		// exec
 		Optional<Customer> value = this.customerServiceImpl.getCustomerById(custumer.getId());
 
 		when(this.customerRepository.findById(value.get().getId())).thenReturn(value);
-       //verification
+		// verification
 		assertEquals("Marcos", value.get().getName());
 
 	}
 
 	@Test(expected = Exception.class)
-	public void saveTest() throws Exception{
-
+	public void saveTest() throws Exception {
+		// Fill data
 		CustomerRequestDto customerRequestDto = new CustomerRequestDto();
 		customerRequestDto.setAddress("Rua A");
 		customerRequestDto.setCpf("12345678911");
@@ -97,26 +98,28 @@ public class CustomerServiceImplTest {
 		cP.setAddress("Rua A");
 		cP.setCpf("12345678911");
 		cP.setName("Marcos");
-
+		//exec
 		when(this.customerServiceImpl.save(customerRequestDto)).thenReturn(cP);
-
-	
+		//verification
+		assertNotNull(cP);
+		assertEquals("Rua A", cP.getAddress());
 
 	}
 
 	@Test(expected = Exception.class)
-	public void saveTestNull() throws Exception{
-		CustomerRequestDto customerRequestDto = mock(CustomerRequestDto.class);
-
+	public void saveTestNull() throws Exception {
+		// Dto instace
+		CustomerRequestDto customerRequestDto = new CustomerRequestDto();
+		//	exec
 		Customer value = this.customerServiceImpl.save(customerRequestDto);
-
+		//	verification
 		assertNull(value);
 
 	}
 
 	@Test(expected = Exception.class)
-	public void updateTest() throws Exception{
-
+	public void updateTest() throws Exception {
+		//	Fill data
 		CustomerRequestDto customerRequestDto = new CustomerRequestDto();
 		customerRequestDto.setAddress("Rua A");
 		customerRequestDto.setCpf("12345678911");
@@ -127,21 +130,25 @@ public class CustomerServiceImplTest {
 		cP.setAddress("Rua A");
 		cP.setCpf("12345678911");
 		cP.setName("Marcos");
-		when(this.customerServiceImpl.update(customerRequestDto,1l)).thenReturn(cP);
-
+		
+		//exec 
+		when(this.customerServiceImpl.update(customerRequestDto, 1l)).thenReturn(cP);
+		//verification
 		assertEquals("Marcos", cP.getName());
 		assertNotNull(cP);
 	}
 
 	@Test(expected = Exception.class)
-	public void updateTestNull() throws Exception{
-		CustomerRequestDto customerRequestDto = mock(CustomerRequestDto.class);
-        Long id =1l;
+	public void updateTestNull() throws Exception {
+		//Dto instace
+		CustomerRequestDto customerRequestDto = new CustomerRequestDto();
+		Long id = 1l;
+		//exec
 		Customer value = this.customerServiceImpl.update(customerRequestDto, id);
+		//verification
 		assertNotEquals("Rua A", customerRequestDto.getAddress());
 		assertNull(value);
 
 	}
-	
-	
+
 }
