e821ac70517e86bcc0311d945c0badc3352f1d81
Marcos Amaral <marcos.professordelphi@gmail.com>
2020-11-01 21:50:09 -0300

    Update WebSecurityConfig.java

--- src/main/java/challenge/dataprev/WebSecurityConfig.java
+++ src/main/java/challenge/dataprev/WebSecurityConfig.java
@@ -1,28 +1,53 @@
 package challenge.dataprev;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private static final String[] AUTH_WHITELIST = {
+			// -- swagger ui
+			"/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+			"/configuration/security", "/swagger-ui.html", "/webjars/**"
+			// other public endpoints of your API may be appended to this array
+	};
 
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/**").permitAll().and().httpBasic() //
-				.and().csrf().disable();
+		
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers(AUTH_WHITELIST).permitAll().anyRequest()
+				.authenticated()
+				
+				.and().formLogin().permitAll();
+		
 	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedMethods("*").allowedHeaders("*").allowedOrigins("*")
-					.allowCredentials(true);
-		}
+		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER").and().withUser("admin")
+				.password("{noop}password").roles("ADMIN");
+
 	}
-	
-	
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
+				"/swagger-ui.*", "/webjars/**");
+	}
+
+}
 
